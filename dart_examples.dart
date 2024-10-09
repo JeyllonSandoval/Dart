8f89e790c1560befb421f617e3aadb3337c2db67
JeyllonSandoval <jeyllomsandoval@gmail.com>
2024-10-08 23:15:46 -0400

    fix - commit error

--- dart_examples.dart
+++ dart_examples.dart
@@ -4,18 +4,4 @@ void main(List<String> args) {
   var apellido = 'Perez';
   print('Hola $nombre $apellido');
 
-    //uso de operadores
-  //var x = null;
-  var a = 10;
-  var b = 20;
-  print('La suma de $a + $b es ${a + b}');
-
-  //uso de condicionales
-  var edad = 18;
-  if (edad >= 18) {
-    print('Eres mayor de edad');
-  } else {
-    print('Eres menor de edad');
-  }
-
 }
