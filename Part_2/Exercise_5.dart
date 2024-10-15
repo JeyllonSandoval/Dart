62144a4e5045d20eab3245a15dec088a88b38caf
JeyllonSandoval <jeyllomsandoval@gmail.com>
2024-10-14 23:40:42 -0400

    feat: Fifth exercise completed, Mixins and Factory

--- Part_2/Exercise_5.dart
+++ Part_2/Exercise_5.dart
@@ -1,9 +1,57 @@
 /*
 Ejercicio 5: Mixins y Factory constructor
-- Crea un mixin Nadador con un método nadar(). Agrégalo a una clase Delfín
-que herede de la clase abstracta Animal.
-- Define un Factory constructor en una clase Vehículo que devuelva una
-instancia de diferentes tipos de vehículos dependiendo del tipo ingresado (e.g.,
-"auto", "moto").
-
+- Crea un mixin Nadador con un método nadar(). Agrégalo a una clase Delfín que herede de la clase abstracta Animal.
+- Define un Factory constructor en una clase Vehículo que devuelva una instancia de diferentes tipos de vehículos dependiendo del tipo ingresado (e.g.,"auto", "moto").
 */
+
+abstract class Animal {
+  void breathe();
+}
+
+mixin Swimmer {
+  void swim() => print('Swimming');
+}
+
+class Dolphin extends Animal with Swimmer {
+  @override
+  void breathe() {
+    print('Breathing');
+  }
+}
+
+class Vehicle {
+  final String type;
+
+  Vehicle(this.type);
+
+  factory Vehicle.create(String type) {
+    switch (type) {
+      case 'auto':
+        return Car();
+      case 'moto':
+        return Motorcycle();
+      default:
+        return Vehicle(type);
+    }
+  }
+}
+
+class Car extends Vehicle {
+  Car() : super('auto');
+}
+
+class Motorcycle extends Vehicle {
+  Motorcycle() : super('moto');
+}
+
+void main() {
+  var dolphin = Dolphin();
+  dolphin.breathe();
+  dolphin.swim();
+
+  var car = Vehicle('auto');
+  print(car.type);
+
+  var motorcycle = Vehicle('moto');
+  print(motorcycle.type);
+}
